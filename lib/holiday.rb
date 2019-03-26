d22c2a72cbfa6c49e6e7cee950df224e42e4ed8c
SOYEONG OH <axionoso@gmail.com>
2019-03-26 15:22:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,6 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
       end
       holiday = final_holiday.join(" ")
       supply = supply.join(", ")
+      puts " #{holiday}: #{supply}"
     end
   end
 end
