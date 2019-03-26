4a96d6db730d5e95d1bcba0d796404a08c548833
SOYEONG OH <axionoso@gmail.com>
2019-03-26 15:26:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,7 +54,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.to_s.capitalize!}:"
 
     holiday_name.each do |holiday, supply|
-      array = holiday.to_s.split("_")
+      holiday_str = holiday.to_s.split("_")
 
       final_holiday = []
       array.each do |supply_item|
